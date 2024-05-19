0adac13ba8d92387d417441a43465c5c21974eb9
raccoonboy <raccoonboy0803@gmail.com>
2024-05-19 22:10:53 +0900

    Feat: Static Image 삽입

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,46 @@
 import React from 'react';
 import Layout from '../../components/layout';
+import { graphql } from 'gatsby';
+import Seo from '../../components/Seo';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
-export default function BlogPost() {
+interface BlogProps {
+  data: Queries.PostDetailQuery;
+  children: any;
+}
+
+export default function BlogPost({ data, children }: BlogProps) {
+  const image = getImage(
+    data.mdx?.frontmatter?.headerImage?.childImageSharp?.gatsbyImageData!
+  );
   return (
-    <Layout title="Blog Post">
-      <div />
+    <Layout title="">
+      <GatsbyImage image={image!} alt={data.mdx?.frontmatter?.title!} />
+      <div>{children}</div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      body
+      frontmatter {
+        author
+        category
+        date
+        title
+        slug
+        headerImage {
+          childImageSharp {
+            gatsbyImageData(height: 450, placeholder: BLURRED)
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }: BlogProps) => (
+  <Seo title={data.mdx?.frontmatter?.title as string} />
+);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import Layout from '../components/layout';
 import Seo from '../components/Seo';
+import { StaticImage } from 'gatsby-plugin-image';
 
 export default function IndexPage() {
   return (
     <Layout title="Welcome to my DevStickers 👋🏻">
-      <div></div>
+      <StaticImage
+        src="https://images.unsplash.com/photo-1625768376503-68d2495d78c5?q=80&w=2825&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Stickers on the wall"
+      />
     </Layout>
   );
 }
